87c0d48b7f94ea431a294c80b532a1de65d9433e
namnh67khmt-lgtm <nam.nh.67khmt@ntu.edu.vn>
2025-11-28 17:09:17 +0700

    Create Nam_Nhuan.py

---
+++ Nam_Nhuan.py
@@ -0,0 +1,6 @@
+year = int(input("Nhập năm: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Năm", year, "là năm nhuận.")
+else:
+    print("Năm", year, "không phải năm nhuận.")
