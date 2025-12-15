ee5f79008415e419775bf8396264cd3930fef540
namnh67khmt-lgtm <nam.nh.67khmt@ntu.edu.vn>
2025-12-15 14:00:09 +0700

    Create TH15_Bai1_Square.py

---
+++ TH15_Bai1_Square.py
@@ -0,0 +1,9 @@
+chieu_dai = int(input("Nhập chiều dài: "))
+chieu_rong = int(input("Nhập chiều rộng: "))
+
+chu_vi = 2 * (chieu_dai + chieu_rong)
+
+dien_tich = chieu_dai * chieu_rong
+
+print("Chu vi hình chữ nhật là:", chu_vi)
+print("Diện tích hình chữ nhật là:", dien_tich)
