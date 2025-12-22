b57255903d2e749d09a3fcc06d863200f52b36d7
namnh67khmt-lgtm <nam.nh.67khmt@ntu.edu.vn>
2025-12-22 15:45:34 +0700

    Create TH2212_Bai5.py

---
+++ TH2212_Bai5.py
@@ -0,0 +1,15 @@
+def tinh_ketqua(n):
+    tu_so = 0
+    for i in range(1, n + 1):
+        tu_so += i
+
+    mau_so = 0
+    for i in range(2, n + 1, 2):
+        mau_so += i
+
+    ket_qua = tu_so / mau_so
+    return ket_qua
+
+n = int(input("Nhập số n: "))
+result = tinh_ketqua(n)
+print(f"Kết quả phép tính là: {result}")
