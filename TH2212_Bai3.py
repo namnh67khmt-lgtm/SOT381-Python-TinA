be5ea8b9fa4765f8e8d9c2f6c919d2420f5bb404
namnh67khmt-lgtm <nam.nh.67khmt@ntu.edu.vn>
2025-12-22 15:12:28 +0700

    Update TH2212_Bai3.py

--- TH2212_Bai3.py
+++ TH2212_Bai3.py
@@ -1,4 +1,4 @@
-def max(a, b, c):
+def tim_max(a, b, c):
     max_value = a
     if b > max_value:
         max_value = b
@@ -6,9 +6,19 @@ def max(a, b, c):
         max_value = c
     return max_value
 
+def tim_min(a, b, c):
+    min_value = a
+    if b < min_value:
+        min_value = b
+    if c > min_value:
+        min_value = c
+    return min_value
+
 a = int(input("Nhập số a: "))
 b = int(input("Nhập số b: "))
 c = int(input("Nhập số c: "))
 
-result = max(a, b, c)
-print(f"Số lớn nhất là: {result}")
+result_max = tim_max(a, b, c)
+result_min = tim_min(a, b, c)
+print(f"Số lớn nhất là: {result_max}")
+print(f"Số nhỏ nhất là: {result_min}")
